9f57da4ee4b1d5a25b0eada3500ed1b5d30c7aef
ala-hamadi <ala.hamadi.01@gmail.com>
2021-07-01 14:30:58 +0200

    initial commit

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA9ajLkF3-Y3JmzmoXvmDarPl-Hs3CmpnM",
+  authDomain: "signal-clone-app-f86ed.firebaseapp.com",
+  projectId: "signal-clone-app-f86ed",
+  storageBucket: "signal-clone-app-f86ed.appspot.com",
+  messagingSenderId: "923943328394",
+  appId: "1:923943328394:web:36a258cb2b38853cc4eb73",
+};
